b17233b32d75e4095550377b2405876c50bae2f0
allenbra17 <allenbra17@gmail.com>
2022-02-24 16:42:43 -0500

    finished product

--- js.js
+++
@@ -1,8 +0,0 @@
-const ley = document.getElementById('flicker')
-
-ley.addEventListener('webkitAnimationEnd', end); 
-
-function end() {
-    ley.parentNode.removeChild(ley);
-    document.getElementById('fname').style.width = '100%';
-  };
